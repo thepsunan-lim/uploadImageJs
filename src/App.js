0ea84d0c6be94fb830b0e9785edec44f1fcfa9be
thepsunan1 <dhepsunan@hotmail.com>
2019-07-22 09:39:32 +0700

    added a comment on ratio

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
       <ComponentTest 
         config={
           {
-            ratio:1,
+            ratio:1, 
           }
         }  
       >
+      {/* the ratio, if set to 0, will let you crop the image freely as limited with minimum size */}
       {/* this is where you config outside the component, the variables should be the same name as in "ExampleCompoent.js" */}  
       </ComponentTest>
     </div>
